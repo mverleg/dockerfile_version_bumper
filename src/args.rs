c0ffeea397b32b96e773fb6b21e999a37d02e4b7
Mark <mverleg.noreply@gmail.com>
2021-12-14 22:18:21 +0100

    Add command line arguments

--- src/args.rs
+++ src/args.rs
@@ -1,3 +1,5 @@
+use ::std::path::PathBuf;
+
 use ::structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
@@ -6,6 +8,10 @@ use ::structopt::StructOpt;
     about = "Simple tool for scanning `Dockerfile`s and switching the image tags that appear in `FROM` to their latest version."
 )]
 pub struct Args {
-    #[structopt(long = "dockerfile", short = "f")]
-    pub evolution_dir: String,
+    #[structopt(long = "dockerfile", short = "f", default_value = "Dockerfile", parse(from_os_str))]
+    pub evolution_dir: Vec<PathBuf>,
+    #[structopt(long = "parent", short = "p", help = "Parent images (FROM lines) base names that should be bumped. If empty, bumps every image in the Dockerfile that is found in the registry.")]
+    pub parent: Vec<String>,
+    #[structopt(long, help = "Allow bumping to new major versions (which might be incompatible), which is interpreted as the leading number in the version.")]
+    pub major: bool,
 }

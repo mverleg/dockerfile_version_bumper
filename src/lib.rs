c0ffeef8e1f106c474d7cfc0a98c033d896a4ddc
Mark <mverleg.noreply@gmail.com>
2021-12-14 23:28:46 +0100

    Take --parent/-p filter flag into account

--- src/lib.rs
+++ src/lib.rs
@@ -15,14 +15,15 @@ lazy_static! {
 
 pub fn bump_dockerfiles(
     dockerfiles: &[PathBuf],
-    parents: &[String],
+    allow_parents: &[String],
     bump_major: bool,
     print: bool,
 ) -> Result<(), String> {
     assert!(bump_major, "bumping only minor versions not implemented, use --major");
     assert!(print, "in-place update not implemented, use --print");
     let dockerfiles = read_all_dockerfiles(dockerfiles)?;
-    let parents = extract_parents(&dockerfiles);
+    let all_parents = extract_parents(&dockerfiles);
+    let parents = filter_parents(all_parents, allow_parents);
     unimplemented!()
 }
 
@@ -112,6 +113,25 @@ fn parse_line_from(line: &str) -> Option<Parent> {
         }
     }
 }
+
+fn filter_parents(all_parents: HashSet<Parent>, allow_parent_names: &[String]) -> Result<HashSet<Parent>, String> {
+    if allow_parent_names.is_empty() {
+        if all_parents.is_empty() {
+            return Err("No FROM tags with versions were found in the Dockerfile(s)".to_owned())
+        }
+        return Ok(all_parents)
+    }
+    let allow_parent_names: HashSet<String> = HashSet::from_iter(allow_parent_names.iter().cloned());
+    let parents = all_parents.into_iter()
+        .filter(|parent| allow_parent_names.contains(&parent.name))
+        .inspect(|parent| debug!("including parent (-p): {}", parent))
+        .collect::<HashSet<_>>();
+    if parents.is_empty() {
+        return Err("None of the FROM tags given with --parent/-p were found in the Dockerfile(s)".to_owned())
+    }
+    Ok(parents)
+}
+
 
 #[cfg(test)]
 mod tests {

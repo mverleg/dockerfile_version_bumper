c0ffee3900cba8f4f6c2a51e110696c454143bc7
Mark <mverleg.noreply@gmail.com>
2022-01-28 23:25:59 +0100

    Updating Dockerfile content

--- src/dvb/write.rs
+++ src/dvb/write.rs
@@ -1,4 +1,6 @@
+use ::std::collections::HashMap;
 use ::std::fs;
+use std::path::PathBuf;
 
 use ::indexmap::IndexMap;
 
@@ -6,12 +8,19 @@ use crate::dvb::data::Tag;
 use crate::Parent;
 
 pub async fn update_all_dockerfiles(latest_tags: &IndexMap<Parent, Tag>) -> Result<(), String> {
-    //TODO @mark: what if multiple updates to same Dockerfile?
-    for (parent, new_tag) in latest_tags.iter() {
-        let content = format!("# updated: {} {} -> {}!\n{}", parent.name(), parent.tag(), new_tag, parent.dockerfile().content());
-        fs::write(parent.dockerfile().path(), content)
-            .map_err(|_| format!("failed to update Dockerfile '{}'", parent.name()))?
-    }
+    let new_content = updated_dockerfiles_content(latest_tags);
     unimplemented!()  //TODO @mark: TEMPORARY! REMOVE THIS!
 }
 
+fn updated_dockerfiles_content(latest_tags: &IndexMap<Parent, Tag>) -> HashMap<PathBuf, String> {
+    //TODO @mark: what if multiple updates to same Dockerfile?
+    let mut files = HashMap::new();
+    for (parent, new_tag) in latest_tags.iter() {
+        let content = files.entry(parent.dockerfile().path()).or_insert_with(|| parent.dockerfile().content());
+        *content = parent.tag_pattern().replace_all(content);
+        // let content = format!("# updated: {} {} -> {}!\n{}", parent.name(), parent.tag(), new_tag, parent.dockerfile().content());
+        // fs::write(parent.dockerfile().path(), content)
+        //     .map_err(|_| format!("failed to update Dockerfile '{}'", parent.name()))?
+    }
+    unimplemented!()  //TODO @mark: TEMPORARY! REMOVE THIS!
+}

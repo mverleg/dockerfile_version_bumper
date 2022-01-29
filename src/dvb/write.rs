c0ffee054dc18ae280dd40c37feb28e9a9b21d6f
Mark <mverleg.noreply@gmail.com>
2022-01-29 22:38:48 +0100

    Attempt to update map inplace

--- src/dvb/write.rs
+++ src/dvb/write.rs
@@ -1,6 +1,7 @@
 use ::std::collections::HashMap;
 use ::std::fs;
-use std::path::PathBuf;
+use ::std::path::PathBuf;
+use std::borrow::Borrow;
 
 use ::indexmap::IndexMap;
 
@@ -16,8 +17,10 @@ fn updated_dockerfiles_content(latest_tags: &IndexMap<Parent, Tag>) -> HashMap<P
     //TODO @mark: what if multiple updates to same Dockerfile?
     let mut files = HashMap::new();
     for (parent, new_tag) in latest_tags.iter() {
-        let content = files.entry(parent.dockerfile().path()).or_insert_with(|| parent.dockerfile().content());
-        *content = parent.tag_pattern().replace_all(content);
+        let content: &mut &String = files.entry(parent.dockerfile().path())
+            .or_insert_with(|| parent.dockerfile().content());
+        let q: String = parent.tag_pattern().replace_all(content, format!("{}", new_tag)).into_owned();
+        *content = q;
         // let content = format!("# updated: {} {} -> {}!\n{}", parent.name(), parent.tag(), new_tag, parent.dockerfile().content());
         // fs::write(parent.dockerfile().path(), content)
         //     .map_err(|_| format!("failed to update Dockerfile '{}'", parent.name()))?
